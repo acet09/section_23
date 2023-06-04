c4b6e7803cb14aa9b654e6463adfeb5f23a8f162
Cactus <ysjungman@gmail.com>
2023-06-04 13:26:07 +0900

    355_페이지간_연결

--- pages/news/index.js
+++ pages/news/index.js
@@ -1,5 +1,20 @@
+import Link from 'next/link';
+import { Fragment } from 'react';
+
 function NewsPage() {
-  return <h1>News Page</h1>
+  return (
+    <Fragment>
+      <h1>News Page</h1>
+      <ul>
+        <li>
+          <Link href='/news/greate-fw'>
+            NextJS Is Great Framework
+          </Link>
+        </li>
+        <li>Something Else</li>
+      </ul>
+    </Fragment>
+  );
 }
 
 export default NewsPage;
